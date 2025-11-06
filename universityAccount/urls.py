6f4f71768b59eb919f8bfd66276f72d61b3ac426
DavitKvinikadze <113443614+DavitKvinikadze@users.noreply.github.com>
2025-11-06 17:15:26 -0500

    removed unneccessary files: "--model; removed test.py from accounts folder and pages folder; removed venvvenv folder; removed user_profile.py;

--- universityAccount/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.university_home, name='university_home'),
-    path('profile/', views.university_profile, name='university_profile'),        # optional
-    path('dashboard/', views.university_dashboard, name='university_dashboard'),  # optional
-]
