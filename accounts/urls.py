48547904fe86e27226892738324f9cd6e036506d
DavitKvinikadze <113443614+DavitKvinikadze@users.noreply.github.com>
2025-11-07 14:56:47 -0500

    fixed ursls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,28 +1,10 @@
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='accounts/password_reset_form.html'
-         ),
-         name='password_reset'),
-    path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='accounts/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='accounts/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='accounts/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
->>>>>>> 51beabdb6478770a4441e74175386d4f01bbe28d
-]
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import RegisterView, CustomLoginView
 
-=======
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/',
          auth_views.PasswordResetView.as_view(
@@ -44,5 +26,4 @@
              template_name='accounts/password_reset_complete.html'
          ),
          name='password_reset_complete'),
->>>>>>> 51beabdb6478770a4441e74175386d4f01bbe28d
 ]
